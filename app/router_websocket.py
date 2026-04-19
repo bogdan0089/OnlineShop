b874c2f04350f06355c96c7771535ba66f2832dc
bohdan0089 <bodiks18@gmail.com>
2026-04-19 13:45:54 +0200

    feat: add token-based auth for WebSocket admin endpoint

--- app/router_websocket.py
+++ app/router_websocket.py
@@ -1,12 +1,23 @@
 from fastapi import WebSocket, APIRouter
 from utils.connection_manager import connection
-
+from services.auth_service import AuthService
+from database.unit_of_work import UnitOfWork
+from core.enum import Role
 
 router_websocket = APIRouter()
 
 
 @router_websocket.websocket("/ws/admin")
-async def websocket_endpoint(websocket: WebSocket):
+async def websocket_endpoint(websocket: WebSocket, token: str):
+    client_id = AuthService.decode_token(token)
+    async with UnitOfWork() as uow:
+        client = await uow.client.get_client(client_id)
+        if not client:
+            await websocket.close()
+            return 
+        if client.role != Role.superadmin:
+            await websocket.close()
+            return 
     await connection.connect(websocket)
     try:
         while True:
